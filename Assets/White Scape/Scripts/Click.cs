ccc6031a98e9061a20422ae671c62e8556bb5547
WangXuan_MacBook <linkovo@Huawei-MateBook-Pro-14.local>
2024-04-22 14:46:07 +0900

    [Script]添加host ap clear按钮
    实现host按钮功能

--- Assets/White Scape/Scripts/Click.cs
+++ Assets/White Scape/Scripts/Click.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Collections;
-using System.Net.Mime;
+using UnityEngine.UI;
 
 public class HighlightTileOnClick : MonoBehaviour
 {
     public Tilemap tilemap; // 引用Tilemap
     public Color highlightColor = Color.yellow; // 高亮颜色，默认为黄色
     private Color originalColor; // 用于存储原始颜色
+    public Tile APTile; // 新的Tile用于替换
+    public Tile HostTile; // Host模式下使用的Tile
     public UnityEngine.UI.Text Location;
+    public Button Clear;
+    public Button Host;
+    public Button AP;
+    private bool isHostMode = false; // 是否处于Host模式
+    private Vector3Int lastClickedPosition; // 存储最后点击的位置
+
     private void Start()
     {
         originalColor = tilemap.color; // 在开始时获取Tilemap的初始颜色
+        
+        Button host = Host.GetComponent<Button>();
+        Button ap = AP.GetComponent<Button>();
+        Button clear = Clear.GetComponent<Button>();
+        
+        ap.onClick.AddListener(APOnClick);
+        host.onClick.AddListener(HostOnClick);
+        clear.onClick.AddListener(ClearOnClick);
     }
 
     private void Update()
@@ -23,10 +39,17 @@ public class HighlightTileOnClick : MonoBehaviour
            
             if (tilemap.HasTile(gridPosition)) // 检测是否点击在有Tile的位置
             {
-                HighlightTile(gridPosition);
+                lastClickedPosition = gridPosition; // 更新最后点击的位置
+                if (isHostMode)
+                {
+                    ChangeToHost(gridPosition);
+                }
+                else
+                {
+                    HighlightTile(gridPosition);
+                }
                 Debug.Log("Tile clicked at position: " + gridPosition);
-                string convert = gridPosition.ToString();
-                Location.text = convert;
+                Location.text = gridPosition.ToString();
             }
         }
     }
@@ -35,6 +58,33 @@ public class HighlightTileOnClick : MonoBehaviour
     {
         tilemap.SetColor(position, highlightColor); // 设置高亮颜色
         StartCoroutine(ResetTileColor(position)); // 延时后重置颜色
+    }
+
+    // 进入AP选择模式
+    void APOnClick()
+    {
+        isHostMode = false; // 关闭Host模式
+        Debug.Log("AP mode activated");
+    }
+
+    // 进入host选择模式
+    void HostOnClick()
+    {
+        isHostMode = true; // 开启Host模式
+        Debug.Log("Host mode activated");
+    }
+
+    void ChangeToHost(Vector3Int position)
+    {
+        tilemap.SetTile(position, HostTile); // 替换为HostTile
+        StartCoroutine(ResetTileColor(position)); // 延时后重置颜色
+    }
+    
+    // 清除
+    void ClearOnClick()
+    {
+        isHostMode = false; // 关闭Host模式
+        Debug.Log("Clear mode activated");
     }
 
     IEnumerator ResetTileColor(Vector3Int position)
