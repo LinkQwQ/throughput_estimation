8b01ed8af3a9f9d72548325192163628262af65d
WangXuan_MacBook <linkovo@Huawei-MateBook-Pro-14.local>
2024-04-26 15:34:54 +0900

    [Script]添加坐标输出逻辑

---
+++ Assets/Throughput_Estimation/Scripts/SaveFile.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class SaveFile : MonoBehaviour
+{
+    // 设定默认的文件路径和头部
+    private static string defaultFilePath = "Assets/Throughput_Estimation/Csv/coordinates_Temp.csv";
+    private static string[] defaultHeaders = new string[] { "Name", "X", "Y", "Type" };
+
+    // 一个公开的静态方法，只需要外部提供数据
+    public static void WriteCSV(string[][] data)
+    {
+        StringBuilder csvContent = new StringBuilder();
+
+        // 添加固定的头部到CSV中
+        csvContent.AppendLine(string.Join(",", defaultHeaders));
+
+        // 遍历数据并添加到CSV内容中
+        foreach (var row in data)
+        {
+            csvContent.AppendLine(string.Join(",", row));
+        }
+
+        // 写入固定的文件路径
+        using (StreamWriter writer = new StreamWriter(defaultFilePath, false)) // 使用using语句自动关闭StreamWriter
+        {
+            writer.Write(csvContent.ToString());
+        }
+    }
+}
