03ec4de22399340e65e3020798341cc24b24cf5e
WangXuan_MacBook <linkovo@Huawei-MateBook-Pro-14.local>
2024-04-28 18:24:10 +0900

    [Script]更改控制模式为键盘

--- Assets/Throughput_Estimation/Scripts/CameraController.cs
+++ Assets/Throughput_Estimation/Scripts/CameraController.cs
@@ -5,44 +5,22 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float moveSpeed = 3f; // 摄像机移动速度
-    public float smoothTime = 0.3f; // 平滑移动的时间
-    private Vector3 velocity = Vector3.zero;
-
     public float zoomSpeed = 1f; // 缩放速度
     public float minZoom = 5f; // 最小缩放值
     public float maxZoom = 20f; // 最大缩放值
 
-    public float boundaryPercentage = 0.05f; // 边界百分比，这里设置为10%
-
     void Update()
     {
-        float boundaryWidth = Screen.width * boundaryPercentage;
-        float boundaryHeight = Screen.height * boundaryPercentage;
+        // 方向键移动
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 mousePosition = Input.mousePosition;
-        bool isWithinBoundary = mousePosition.x < boundaryWidth || mousePosition.x > Screen.width - boundaryWidth ||
-                                mousePosition.y < boundaryHeight || mousePosition.y > Screen.height - boundaryHeight;
-
-        if (isWithinBoundary)
-        {
-            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            targetPosition.z = transform.position.z; // 2D 游戏没有Z轴的数值
-
-            // 使用SmoothDamp平滑移动到目标位置
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-        }
-        else
-        {
-            // 重置速度向量，消除惯性
-            velocity = Vector3.zero;
-        }
+        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
         // 滚轮缩放
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (scroll != 0)
-        {
-            Camera.main.orthographicSize -= scroll * zoomSpeed;
-            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minZoom, maxZoom);
-        }
+        Camera.main.orthographicSize -= scroll * zoomSpeed;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minZoom, maxZoom);
     }
 }
